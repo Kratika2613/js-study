1e2cb02ef8af5e8a1fcaf575218c9ec924afa748
Kratika Pachori <142537126+Kratika2613@users.noreply.github.com>
2025-01-07 14:12:37 +0000

    comparison of dataTypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -16,8 +16,31 @@ let score = true
 //"" => false
 //"kratika" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************** Operations ****************
+
+let value = 3
+let negVlaue = -value
+// console.log(negVlaue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);// ** -> 2 the power 4
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " kratika"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// let gameCounter = 200
+// ++gameCounter;
+// console.log(gameCounter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//=== --> it not only checks value strictly but also 
+        //checks its data types.
+
+console.log("2" === 2);
+console.log(2 === 2);
